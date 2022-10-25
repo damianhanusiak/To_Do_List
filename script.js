10865a1b0c0e2a5e528850bf9e0c5e19b2665d72
Damian Hanusiak <hanusiak.damian@gmail.com>
2022-10-25 19:22:33 +0200

    script done

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let taskInput = document.querySelector(".task-input");
-let error = document.querySelector(".error");
-let addBtn = document.querySelector(".button-add");
-let ulList = document.querySelector(".tasks-list ul");
+const taskInput = document.querySelector(".task-input");
+const error = document.querySelector(".error");
+const addBtn = document.querySelector(".button-add");
+const ulList = document.querySelector(".tasks-list ul");
 let newTask;
+
+const editWindow = document.querySelector(".edit-window");
+const editWindowInfo = document.querySelector(".edit-window-info");
+const editInput = document.querySelector(".edit-input");
+const editAddBtn = document.querySelector(".accept");
+const editCloseBtn = document.querySelector(".cancel");
+let taskToEdit;
 
 const addNewTask = () => {
 	if (taskInput.value !== "") {
@@ -37,12 +44,51 @@ const createTools = () => {
 const toolsHandling = e => {
 	if (e.target.matches(".delete")) {
 		deleteTask(e);
+	} else if (e.target.matches(".complete")) {
+		e.target.closest("li").classList.toggle("completed");
+		e.target.classList.toggle("completed");
+	} else if (e.target.matches(".edit")) {
+		editTask(e);
 	}
 };
 
 const deleteTask = e => {
 	e.target.closest("li").remove();
+	const allTasks = ulList.querySelectorAll("li");
+	if (allTasks.length === 0) {
+		error.textContent = "No tasks on the list!";
+	}
+};
+
+const editTask = e => {
+	editWindow.style.display = "flex";
+	taskToEdit = e.target.closest("li");
+	editInput.value = taskToEdit.firstChild.textContent;
+};
+
+const closeEditWindow = () => {
+	editWindow.style.display = "none";
+	editWindowInfo.textContent = "";
+};
+
+const editTaskText = () => {
+	if (editInput.value !== "") {
+		taskToEdit.firstChild.textContent = editInput.value;
+		editWindow.style.display = "none";
+		editWindowInfo.textContent = "";
+	} else {
+		editWindowInfo.textContent = "You need to add some text!";
+	}
+};
+
+const addTaskViaEnter = e => {
+	if (e.key === "Enter") {
+		addNewTask();
+	}
 };
 
 addBtn.addEventListener("click", addNewTask);
 ulList.addEventListener("click", toolsHandling);
+editCloseBtn.addEventListener("click", closeEditWindow);
+editAddBtn.addEventListener("click", editTaskText);
+taskInput.addEventListener("keyup", addTaskViaEnter);
